9545da9b340b6d84376a63c675e400b9ec23e451
ilyas <ilyasouagag2011@gmail.com>
2023-09-11 05:00:01 +0100

    task 1 && 2

--- shell.c
+++ shell.c
@@ -8,11 +8,13 @@
  */
 int main(int argc, char **argv)
 {
-	char *line = NULL, **arguments = NULL, pipe_input[1024];
+	char *line = NULL, **arguments = NULL;
+	char pipe_input[1024];
 	size_t len = 0;
+	size_t longueur;
 	ssize_t lenght = 0;
-	ssize_t scan = 0;
-	int status = 0, alert = 0, j;
+
+	int status = 0, alert, j;
 	(void)argc;
 	(void)argv;
 
@@ -29,34 +31,35 @@ int main(int argc, char **argv)
 				return (status);
 			}
 			line[lenght - 1] = '\0';
-
 			arguments = split_line(line, 1); /* MEMORY TO BE FREED */
 			status = execution(arguments, line, 1);
 		}
-		else if (isatty(STDIN_FILENO) == 0)
+		else if (!isatty(STDIN_FILENO))
 		{
-			scan = getline(&line, &len, stdin); /* Memory allocated for line */
-			if (scan == -1)
-			{
-				free(line), line = NULL; /* FREE ALLOCATED FOR LINE */
-				putchar('\n');
-				return (status);
-			}
-			for (j = 0; line[j] != '\0'; j++)
-			{
-				if (line[j] != 32)
-				{
-					alert = 1;
-					break;
-				}
-			}
-			if (alert == 0)
-			{
-				free(line);
-				return 0;
-			}
 			while (fgets(pipe_input, sizeof(pipe_input), stdin)) /* DOESN'T ALLOCATE THE MEMORY */
 			{
+				longueur = strlen(pipe_input);
+				if (longueur > 0 && pipe_input[longueur - 1] == '\n')
+				{
+					pipe_input[longueur - 1] = '\0';
+				}
+				alert = 0;
+				for (j = 0; pipe_input[j] != '\0'; j++)
+				{
+					if (pipe_input[j] != ' ')
+					{
+						alert = 1;
+						break;
+					}
+				}
+				if (alert == 0)
+				{
+					continue;
+				}
+				if(_strncmp(pipe_input,"exit",4) == 0)
+				{
+					return(status);
+				}
 				arguments = split_line(pipe_input, 0);
 				status = execution(arguments, pipe_input, 0);
 			}
