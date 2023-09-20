a23a386e718d252fb61b4acf9ecc800f0b13c4e5
Ismail <im179242@gmail.com>
2023-09-20 20:07:22 +0100

    Fix func

--- un_setenv.c
+++ un_setenv.c
@@ -93,7 +93,7 @@ return (0);
 }
 /**
 *_realloc - reallocates a memory block using malloc and free
-* @ptr: pointer to the memory previsouly allocated by malloc
+* @ptr: pointer to the memory 
 * @old_size: size of the allocated memory for ptr
 * @new_size: new size of the new memory block
 * Return: pointer to the newly allocated memory block
